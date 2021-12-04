2fd4afcbf56d73475ea379dc91a1c9f3604bf076
Erich Gubler <erichdongubler@gmail.com>
2021-12-04 10:15:05 -0500

    fixup! feat: finish day 3, part 1

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -18,33 +18,50 @@ pub const EXAMPLE: &str = "\
 ";
 
 pub fn calculate_gamma(input: &str) -> (u32, usize) {
-    let lines = input.lines().map(|l| l.trim().chars().map(|c| { if !matches!(c, '0' | '1') {
-        panic!("blarg invalid character");
-    } c }));
+    let lines = input.lines().map(|l| {
+        l.trim().chars().map(|c| {
+            if !matches!(c, '0' | '1') {
+                panic!("blarg invalid character");
+            }
+            c
+        })
+    });
 
-    let line_len = lines.clone().next().context("y u no have line").unwrap().count();
+    let line_len = lines
+        .clone()
+        .next()
+        .context("y u no have line")
+        .unwrap()
+        .count();
 
     assert!(line_len <= size_of::<u32>() * 8);
     let mut running_avgs = vec![0i32; line_len];
 
-    lines.flat_map(|cs| cs.enumerate()).fold(line_len - 1, |last_idx, (idx, c)| {
-        if idx == 0 && last_idx != (line_len - 1) {
-            panic!("lines varied in length, boo");
-        }
+    lines
+        .flat_map(|cs| cs.enumerate())
+        .fold(line_len - 1, |last_idx, (idx, c)| {
+            if idx == 0 && last_idx != (line_len - 1) {
+                panic!("lines varied in length, boo");
+            }
 
-        running_avgs[idx] = running_avgs[idx].checked_add(match c {
-            '0' => -1,
-            '1' => 1,
-            c => panic!("blarg invalid character {:?}", c),
-        }).expect("wat, {under,over}flow?");
+            running_avgs[idx] = running_avgs[idx]
+                .checked_add(match c {
+                    '0' => -1,
+                    '1' => 1,
+                    c => panic!("blarg invalid character {:?}", c),
+                })
+                .expect("wat, {under,over}flow?");
 
-        idx
-    });
+            idx
+        });
 
-    let gamma = running_avgs.iter().fold(0, |gamma, &bucket| (gamma << 1) | match bucket.cmp(&0) {
-        Ordering::Equal => panic!("IDK what to do with this man"),
-        Ordering::Greater => 1,
-        Ordering::Less => 0,
+    let gamma = running_avgs.iter().fold(0, |gamma, &bucket| {
+        (gamma << 1)
+            | match bucket.cmp(&0) {
+                Ordering::Equal => panic!("IDK what to do with this man"),
+                Ordering::Greater => 1,
+                Ordering::Less => 0,
+            }
     });
     (gamma, line_len)
 }
@@ -69,5 +86,8 @@ pub const INPUT: &str = include_str!("day3_input.txt");
 #[test]
 fn part1() {
     let (gamma, gamma_width) = calculate_gamma(INPUT);
-    assert_eq!(calculate_submarine_power_consumption(gamma, gamma_width), 2003336);
+    assert_eq!(
+        calculate_submarine_power_consumption(gamma, gamma_width),
+        2003336
+    );
 }
